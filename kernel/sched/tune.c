54c8756c4bf6a7e72995f696958877ef3528f2da
willoong9559 <501801418@qq.com>
2020-06-18 06:19:28 +0800

    CpuAssist & StuneAssist: Tweak values and add schedtune.sched_boost s…

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -1,3 +1,4 @@
+#include <linux/binfmts.h>
 #include <linux/cgroup.h>
 #include <linux/err.h>
 #include <linux/kernel.h>
@@ -731,12 +732,12 @@ static struct cftype files[] = {
 	{
 		.name = "boost",
 		.read_s64 = boost_read,
-		.write_s64 = boost_write,
+		.write_s64 = boost_write_wrapper,
 	},
 	{
 		.name = "prefer_idle",
 		.read_u64 = prefer_idle_read,
-		.write_u64 = prefer_idle_write,
+		.write_u64 = prefer_idle_write_wrapper,
 	},
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	{
@@ -771,6 +772,45 @@ schedtune_boostgroup_init(struct schedtune *st)
 	return 0;
 }
 
+#ifdef CONFIG_STUNE_ASSIST
+struct st_data {
+	char *name;
+	int boost;
+	bool prefer_idle;
+	int sched_boost;
+};
+
+static void write_default_values(struct cgroup_subsys_state *css)
+{
+	static struct st_data st_targets[] = {
+		{ "top-app",	1, 1, 10 },
+		{ "foreground",	0, 1,  0 }
+	};
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(st_targets); i++) {
+		struct st_data tgt = st_targets[i];
+
+		if (!strcmp(css->cgroup->kn->name, tgt.name)) {
+			pr_info("stune_assist: setting values for %s: boost=%d prefer_idle=%d sched_boost=%d\n",
+				tgt.name, tgt.boost, tgt.prefer_idle);
+
+			boost_write(css, NULL, tgt.boost);
+			prefer_idle_write(css, NULL, tgt.prefer_idle);
+			sched_boost_write(css, NULL, tgt.sched_boost);
+		}
+	}
+}
+#endif
+
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+static void filterSchedtune(struct schedtune *sti, struct schedtune **sto_p, char *st_name)
+{
+	if (!strncmp(sti->css.cgroup->kn->name, st_name, strlen(st_name)))
+		*sto_p = sti;
+}
+#endif
+
 static struct cgroup_subsys_state *
 schedtune_css_alloc(struct cgroup_subsys_state *parent_css)
 {
@@ -786,10 +826,13 @@ schedtune_css_alloc(struct cgroup_subsys_state *parent_css)
 		return ERR_PTR(-ENOMEM);
 	}
 
-	/* Allow only a limited number of boosting groups */
-	for (idx = 1; idx < BOOSTGROUPS_COUNT; ++idx)
+	for (idx = 1; idx < BOOSTGROUPS_COUNT; ++idx) {
 		if (!allocated_group[idx])
 			break;
+#ifdef CONFIG_STUNE_ASSIST
+		write_default_values(&allocated_group[idx]->css);
+#endif
+	}
 	if (idx == BOOSTGROUPS_COUNT) {
 		pr_err("Trying to create more than %d SchedTune boosting groups\n",
 		       BOOSTGROUPS_COUNT);
